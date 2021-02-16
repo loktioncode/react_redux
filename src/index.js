55b5eb957bbab1ada146229aba0433f9729ef414
DESKTOP-TEK06LE\EBere <elisha.b@cestasoft.co.za>
2021-02-16 12:36:43 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+
+
+const store = createStore(allReducers,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   );
 
 ReactDOM.render(
   <React.StrictMode>
